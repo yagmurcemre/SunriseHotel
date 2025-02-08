e8ca72af1be95c4f5091d4305079768bfd79671f
yagmurcemre <yagmurcemreyamali3@gmail.com>
2025-02-08 16:58:08 +0300

    a

--- SunriseHotel/Program.cs
+++ SunriseHotel/Program.cs
@@ -9,9 +9,7 @@ namespace SunriseHotel
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
